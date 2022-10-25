cf8ee43c5ac9d89e584815604a301a37f0637782
ldilz <108256579+lahirudilz@users.noreply.github.com>
2022-10-25 15:22:06 +0530

    hello-world app created

---
+++ src/main/java/com/example/helloworld/HelloController.java
@@ -0,0 +1,15 @@
+package com.example.helloworld;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/")
+public class HelloController {
+
+    @GetMapping(path = "hello-world/welcome")
+    public String welcome(){
+        return "Welcome to LOIT";
+    }
+}
